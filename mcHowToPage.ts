cbafc3f66c92d17459f04c901288db86c7f4e50d
Samantha Trevino <samanthatrevino@Samanthas-Air.lan>
2023-09-13 20:46:51 -0500

    sams push

--- mcHowToPage.ts
+++ mcHowToPage.ts
@@ -2,8 +2,10 @@ import { By } from "selenium-webdriver";
 import {BasePage} from './basePage';
 
 export class Mcck extends BasePage {
-    kitchenHowTos: By = By.xpath('//a[@target="_self"]')
-    seeAllArticles: By = By.xpath('//a[text()="See All"]')
+    kitchenHowTos: By = By.xpath('//li[@class="label1 item_3 has-subnav subnav-open"]')
+    seeAllArticles: By = By.xpath('(//a[text()="See All"])[1]')
+    waysToGroundBeef: By = By.xpath('//a[@class="module tabbed-banner-module grey-bg-overlay bgloaded"][1]')
+    
 
     constructor() {
         super({url: 'https://www.mccormick.com/'})
