1eb8333fa056e6a8ca4bb9618569bac407e6b84e
Jer w <jeremiahwwinters@gmail.com>
2023-09-13 20:56:09 -0600

    test 2

--- ChefJ.test.ts
+++ ChefJ.test.ts
@@ -3,26 +3,20 @@ import { mccormickBP } from "./mccormickBP";
 const mcc = new mccormickBP()
 const fs = require("fs")
 
-test('Banana Muffin Recipe loads correctly', async () => {
+test('Chef J Favorite Recipe Loads', async () => {
     await mcc.navigate()
     await mcc.driver.manage().window().maximize()
-    await mcc.click(mcc.Recipes)
-    await mcc.driver.sleep(2000)
-    await mcc.click(mcc.ARecipes)
-    await mcc.driver.sleep(2000)
-    await mcc.click(mcc.QBreakfast)
+    await mcc.click(mcc.KitchenHT)
+    await mcc.click(mcc.MeetChef)
     await mcc.driver.sleep(2000)
     await mcc.click(mcc.CancelBtn)
     await mcc.driver.sleep(2000)
-    await mcc.click(mcc.DonutsBG)
+    await mcc.click(mcc.ChefJ)
     await mcc.driver.sleep(2000)
-    await mcc.click(mcc.SeeMore)
+    await mcc.click(mcc.BTacos)
     await mcc.driver.sleep(2000)
-    await mcc.click(mcc.BananaNM)
-    await mcc.driver.sleep(2000)
-    await mcc.click(mcc.FullRecipe)
-    await mcc.driver.sleep(2000)
-    await fs.writeFile(`${__dirname}/bananamuffinrecipe.png`,
+    await mcc.click(mcc.ViewRecO)
+    await fs.writeFile(`${__dirname}/ChefJRecipe.png`,
     await mcc.driver.takeScreenshot(), "base64",
     (e) => {
         if (e) console.error(e)
