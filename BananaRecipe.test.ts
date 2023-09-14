dfa6860618b81c8ddb9512838e0f01ef67d3d4e5
Jer w <jeremiahwwinters@gmail.com>
2023-09-13 19:18:04 -0600

    2nd Push

--- BananaRecipe.test.ts
+++ BananaRecipe.test.ts
@@ -1,16 +1,32 @@
 import { mccormickBP } from "./mccormickBP";
 
 const mcc = new mccormickBP()
+const fs = require("fs")
 
 test('Banana Muffin Recipe loads correctly', async () => {
     await mcc.navigate()
     await mcc.driver.manage().window().maximize()
     await mcc.click(mcc.Recipes)
+    await mcc.driver.sleep(2000)
     await mcc.click(mcc.ARecipes)
+    await mcc.driver.sleep(2000)
     await mcc.click(mcc.QBreakfast)
+    await mcc.driver.sleep(2000)
+    await mcc.click(mcc.CancelBtn)
+    await mcc.driver.sleep(2000)
     await mcc.click(mcc.DonutsBG)
+    await mcc.driver.sleep(2000)
     await mcc.click(mcc.SeeMore)
+    await mcc.driver.sleep(2000)
     await mcc.click(mcc.BananaNM)
+    await mcc.driver.sleep(2000)
     await mcc.click(mcc.FullRecipe)
-    
+    await mcc.driver.sleep(2000)
+    await fs.writeFile(`${__dirname}/bananamuffinrecipe.png`,
+    await mcc.driver.takeScreenshot(), "base64",
+    (e) => {
+        if (e) console.error(e)
+        else console.log('Save Succesful')
+    }
+    )
 })
--- mccormickBP.ts
+++ mccormickBP.ts
@@ -4,10 +4,10 @@ import {BasePage} from "./basePage"
 export class mccormickBP extends BasePage {
     Recipes: By = By.xpath('//a[text()="Recipes"]')
     ARecipes: By = By.xpath('//a[text()="View All Recipes"]')
-    QBreakfast: By = By.xpath('(//li[@class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6 recipe-box-gallery-cont show"])[4]')
+    QBreakfast: By = By.xpath('(//img[@*])[15]')
     DonutsBG: By = By.xpath('//a[text()="Donuts & Baked Goods"]')
     CancelBtn: By = By.xpath('//*[@id="ow-regitration-form"]/div[3]/button')
-    SeeMore: By = By.xpath('//*[@id="tab-see-more-recipe"]/text()')
+    SeeMore: By = By.xpath('//a[@id="tab-see-more-recipe"]')
     BananaNM: By = By.xpath('(//div[@class="ow-square-recipe-card clickable-card"])[9]')
     FullRecipe: By = By.xpath('//button[@class="ow-recipe-video-button ow-view-recipe-btn"]')
 
