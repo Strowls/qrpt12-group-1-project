cf8ab34f7ad6e7979ed96299c278a4a22d819918
Samantha Trevino <samanthatrevino@Samanthas-MacBook-Air.local>
2023-09-15 11:38:49 -0500

    sam got 2 tests to pass

--- GBArticle.test.ts
+++ GBArticle.test.ts
@@ -1,5 +1,6 @@
 import {mcHowToPage} from './mcHowToPage'
 const mcck = new mcHowToPage ();
+const fs = require("fs")
 
 test('find 3 ways to ground beef article', async () => {
     await mcck.navigate()
@@ -9,13 +10,14 @@ test('find 3 ways to ground beef article', async () => {
     await mcck.driver.sleep(2000)
     await mcck.click(mcck.seeAllArticles)
     await mcck.driver.sleep(2000)
-    await mcck.click(mcck.cancelBtn)
     await mcck.click(mcck.waysToGB)
+    await mcck.click(mcck.cancelBtn)
     await mcck.driver.sleep(2000)
+    await fs.writeFile(`${__dirname}/threeWaysToGBRecipe.png`,
     await mcck.driver.takeScreenshot(), "base64",
     (e) => {
         if (e) console.error(e)
         else console.log('Save Succesful')
-    }
+    })
     await mcck.driver.quit()
-})
+});
