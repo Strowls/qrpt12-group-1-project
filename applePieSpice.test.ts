271208e304f969bba6dd16b86de9fd424abdbc90
Shawn Rowley <shawnrowley@Shawn-Rowley-C02HD10CQ05N.local>
2023-09-16 09:34:06 -0600

    adding screen maximize

--- applePieSpice.test.ts
+++ applePieSpice.test.ts
@@ -3,6 +3,7 @@ const mccSR = new mccormickPage
 
 test ('Apple Pie Spice test',async () => {
     await mccSR.navigate();
+    await mccSR.driver.manage().window().maximize()
     await mccSR.click(mccSR.products);
     await mccSR.driver.sleep(2000);
     await mccSR.click(mccSR.spices);
--- pepperGrinder.test.ts
+++ pepperGrinder.test.ts
@@ -3,6 +3,7 @@ const mccSR = new mccormickPage
 
 test ('Pepper Grinder Test',async () => {
     await mccSR.navigate();
+    await mccSR.driver.manage().window().maximize()
     await mccSR.click(mccSR.products);
     await mccSR.click(mccSR.grinders);
     await mccSR.click(mccSR.pepperGrinder);
