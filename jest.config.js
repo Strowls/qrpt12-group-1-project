cbdcb0ae4b496809fe5dea8f5157926ac19204b6
Samantha Trevino <samanthatrevino@Samanthas-Air.lan>
2023-09-16 10:00:04 -0500

    2 test done

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-<<<<<<< HEAD
-  testTimeout: 200000,
-=======
   testTimeout: 60000,
->>>>>>> f36cc7692ab91cd1ce14f6e2bd0f1e37d99526b1
 };
